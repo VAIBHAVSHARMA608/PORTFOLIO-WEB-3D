68897c674860ce6b8f6c03fd0d296bc5766a986b
VAIBHAV SHARMA <vaibhavattri77@gmail.com>
2026-06-24 12:52:04 -0700

    Refactor RootComponent to use Outlet directly

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,30 +1,8 @@
 import { QueryClient } from "@tanstack/react-query";
-import {
-  Outlet,
-  createRootRouteWithContext,
-} from "@tanstack/react-router";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
 }>()({
-  component: RootComponent,
+  component: () => <Outlet />,
 });
-
-function RootComponent() {
-  return (
-    <div
-      style={{
-        background: "black",
-        color: "lime",
-        minHeight: "100vh",
-        padding: "40px",
-        fontSize: "32px",
-      }}
-    >
-      <h1>WEB LORD TEST</h1>
-      <p>If you can see this, root route is rendering.</p>
-
-      <Outlet />
-    </div>
-  );
-}
